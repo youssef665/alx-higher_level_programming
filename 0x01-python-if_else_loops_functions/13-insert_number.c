5b919c6c6275b56f4656fdb933b3b05fd7d00756
youssef665 <yi559668@gmail.com>
2023-08-16 23:41:54 -0700

    linked lists

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,26 +9,32 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node1 = *h, *newn;
+	listint_t *node1 = *head, *newn;
 
 	newn = malloc(sizeof(listint_t));
 	if (newn == NULL)
 		return (NULL);
+
 	newn->n = number;
 
 	if (node1 == NULL || node1->n >= number)
 	{
-		new->next = node1;
-		*h = newn;
+		newn->next = node1;
+
+		*head = newn;
+
 		return (newn);
 	}
+
+
 
 	while (node1 && node1->next && node1->next->n < number)
 		node1 = node1->next;
 
 	newn->next = node1->next;
-	n1ode->next = newn;
+	node1->next = newn;
+
+
 
 	return (newn);
 }
-
