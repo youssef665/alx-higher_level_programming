0cf8d8a6f74803278ee6e001fa3540df8fce157f
youssef665 <yi559668@gmail.com>
2023-08-28 22:38:05 -0700

    8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,7 +7,8 @@ def safe_function(fct, *args):
     try:
         r = fct(*args)
         return (r)
-    except ImportError:
+    except:
+
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
 
         return (None)
