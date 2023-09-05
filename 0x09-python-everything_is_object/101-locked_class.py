181e84ea42a2856922260a889b9636921777544d
youssef665 <yi559668@gmail.com>
2023-09-05 05:52:53 -0700

    13

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Defines  LockedClass"""
+
+
+class LockedClass:
+    """
+    Prevents the user from creating new instance 
+
+    Attributes:
+        first_name (str): first name
+    """
+
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """Creates new instances """
+
+
+        self.first_name = "first_name"
