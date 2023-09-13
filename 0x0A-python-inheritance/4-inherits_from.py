d3760c67b0021aab28d2d6db1d942db6e9abe623
youssef665 <yi559668@gmail.com>
2023-09-13 04:58:19 -0700

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,6 +7,6 @@ module with meth inherits_from
 
 def inherits_from(obj, a_class):
     """Methid checks if an object is an instance of a class
-    that inherited  from directly or undirectly""" 
+    that inherited  from directly or undirectly"""
 
     return False if type(obj) is a_class else isinstance(obj, a_class)
