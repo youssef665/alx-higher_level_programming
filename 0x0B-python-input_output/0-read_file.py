f55ae90915869633c1afc63827dac9a7ecd84b32
youssef665 <yi559668@gmail.com>
2023-09-13 11:20:38 -0700

    1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""Defines  text file read only function."""
-
 
 def read_file(filename=""):
 
-    """Print the contents of a file."""
-    with open(filename,"r", encoding="UTF-8") as f:
+    with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
