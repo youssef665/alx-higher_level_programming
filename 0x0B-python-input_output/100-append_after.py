d26556a5b4b85c03c1a3ddb729360a03b032f20e
youssef665 <yi559668@gmail.com>
2023-09-13 11:11:49 -0700

    100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines text file insert function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line.
+
+    Args:
+        filename (str): name of file
+        search_string (str): The string to search
+        new_string (str): The string to add
+    """
+
+    text = ""
+
+    with open(filename) as s:
+        for l in s:
+            t += l
+
+            if search_string in l:
+                t += new_string
+
+    with open(filename, "w") as wr:
+        wr.write(t)
