c2deb245754d866407eb044de83e1186b6c5d8b0
youssef665 <yi559668@gmail.com>
2023-09-13 11:23:34 -0700

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-
+"""ADd all arguments to pytohon list"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """ save to python list function
+        filename: the file name
+    """
 
     with open(filename, "w", encoding="UTF-8") as f:
         json.dump(my_obj, f)
